1a05a9b7fa49cbe78659e8e6d75266513ce6d286
DoravanteBeto <41833666+doravantebeto@users.noreply.github.com>
2020-05-23 12:08:39 -0300

    Remove the now ignored directory src/config

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-const databaseUser = "admin"
-
-const databasePass = "keRTktetTKF1loIB"
-
-const databaseURL = `mongodb+srv://${databaseUser}:${databasePass}@login-api-jwt-zpi4v.gcp.mongodb.net/test?retryWrites=true&w=majority`
-
-module.exports = databaseURL
